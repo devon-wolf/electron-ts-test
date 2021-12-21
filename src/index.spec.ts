7b95567601e01177a51814b07c36afa090dae2b0
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-12-20 17:46:06 -0800

    remove unused files and packages, update package.json

--- src/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import * as indexModule from './index';
-
-it('exists', () => {
-  expect(indexModule).toBeTruthy();
-});
-
-it('says hello', () => {
-  expect(indexModule.sayHello()).toEqual('Hello, world');
-});
